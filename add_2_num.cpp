99f05a3c40a4c1fc2c3c6a6d1b9a33fbf9f55994
orpdoji <wyuan024@ucr.edu>
2022-09-23 22:16:20 -0700

    Edit 1, attempt 1 at the problem

--- add_2_num.cpp
+++ add_2_num.cpp
@@ -1,30 +1,30 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
+ 
 class Solution {
+     //Definition for singly-linked list.
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int num1, num2;
         int mult1 = 1;
         for(ListNode * temp = l1; temp->next != nullptr; temp = temp->next){
-            num1 += (temp->val)*mult;
+            num1 += (temp->val)*mult1;
             mult1*=10;
         }
         int mult2=1;
         for(ListNode * temp = l2; temp->next != nullptr; temp = temp->next){
-            num2 += (temp->val)*mult;
+            num2 += (temp->val)*mult2;
             mult2*=10;
         }
         int sum = num1 + num2;
         ListNode * res;
-        int mult = -1
+        int mult = -1;
         if(mult1>mult2){
             mult = mult1;
         }
@@ -32,7 +32,10 @@ public:
             mult = mult2;
         }
         for(ListNode * temp = res; mult !=1; mult/=10){
-            
+            temp->val = sum % mult;
+            ListNode * temp2;
+            temp -> next = temp2;
+            temp = temp2;
         }
     }
 };
