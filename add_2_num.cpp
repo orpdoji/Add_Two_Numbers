3c675fa82d154e098a7b83c6b33b09946cfc239a
orpdoji <wyuan024@ucr.edu>
2022-09-26 10:05:13 -0700

    Finished the problem, work as intended

--- add_2_num.cpp
+++ add_2_num.cpp
@@ -1,7 +1,5 @@
-
- 
-class Solution {
-     //Definition for singly-linked list.
+#include <iostream>
+using namespace std;
  struct ListNode {
      int val;
      ListNode *next;
@@ -9,33 +7,63 @@ class Solution {
      ListNode(int x) : val(x), next(nullptr) {}
      ListNode(int x, ListNode *next) : val(x), next(next) {}
  };
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int num1, num2;
-        int mult1 = 1;
-        for(ListNode * temp = l1; temp->next != nullptr; temp = temp->next){
-            num1 += (temp->val)*mult1;
-            mult1*=10;
-        }
-        int mult2=1;
-        for(ListNode * temp = l2; temp->next != nullptr; temp = temp->next){
-            num2 += (temp->val)*mult2;
-            mult2*=10;
-        }
-        int sum = num1 + num2;
-        ListNode * res;
-        int mult = -1;
-        if(mult1>mult2){
-            mult = mult1;
-        }
-        else{
-            mult = mult2;
-        }
-        for(ListNode * temp = res; mult !=1; mult/=10){
-            temp->val = sum % mult;
-            ListNode * temp2;
-            temp -> next = temp2;
-            temp = temp2;
-        }
+
+ListNode* add2sum(ListNode* l1, ListNode* l2) {
+
+    int num1=0;
+    int num2=0;
+    int mult1 = 1;
+    int mult2=1;
+
+    for(ListNode * temp = l1; temp != nullptr; temp = temp->next){
+        num1 += (temp->val)*mult1;
+        mult1*=10;
     }
-};
+
+
+
+    for(ListNode * temp = l2; temp != nullptr; temp = temp->next){
+        num2 += (temp->val)*mult2;
+        mult2*=10;
+    }
+
+    int sum = num1 + num2;
+    int mult = 1;
+    int c = -1;
+    while(c!=sum){
+        c = sum % mult;
+        mult*=10;
+    }
+    mult/=10;
+    cout<<sum<<endl;
+    ListNode * ret = new ListNode();
+    for(ListNode*temp = ret; mult!=1; mult/=10){
+        if(mult==10){
+            int temp2 = sum;
+            temp->val = temp2;
+            cout<<temp2<<endl;
+            break;
+        }
+        int temp2 = sum%(10);
+        cout<<temp2<<endl;
+        temp->val = temp2;
+        sum-=temp2;
+        sum/=10;
+        temp->next = new ListNode();
+        temp = temp->next;
+    }
+    return ret;
+}
+int main(){
+    ListNode * l1 = new ListNode(9);
+    l1->next = new ListNode(9);
+    l1->next->next = new ListNode(9);
+    l1->next->next->next = new ListNode(9);
+    l1->next->next->next->next = new ListNode(9);
+
+    ListNode * l2 = new ListNode(9);
+    l2->next = new ListNode(9);
+    l2->next->next = new ListNode(9);
+
+    ListNode * l3 = add2sum(l1,l2);
+}
